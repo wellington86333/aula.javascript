7fdd68b3d669661917f0752f4c61b56e64a49f9a
WellingtoBrito <wellington.brito@rocketmail.com>
2021-08-01 00:41:37 -0300

    primeiro dia de aula js

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var d = new Date();
+ alert(d.getMonth());
+ alert(d.getHours());
+ alert(d.getMinutes());
+
+/*var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+*/
+
+
+
+
+/*var count = 0;
+while(count <= 5){
+    alert(count);
+    count ++;
+}
+*/
+
+/*var idade = prompt("Qual sua idade");
+if (idade >= 18){
+    alert("maior de idade")
+}else{
+    alert("menor de idade")
+}
+*/
+
+
+
+/*var frutas = {[nome: "maçã", cor: "vermelha"}];
+alert(frutas[1].nome);
+
+
+/* var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta);
+alert(fruta.cor);
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva")
+//lista.pop("laranja")
+//console.log(lista[1])
+//alert(lista[1])
+//console.log(lista.toString())
+//console.log(lista.reverse())
+//console.log(lista.join(" /"));
+
+
+
+//var nome = "Wellington Brito";
+//var idade = 34;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + "Tem" + idade + "anos")
+//alert(idade + idade2)
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão, Brasil"));
+//alert(frase.replace("Japão", "Brasil"))
+//console.log(frase.toLowerCase);
+//console.log(frase.toUpperCase);
+*/
